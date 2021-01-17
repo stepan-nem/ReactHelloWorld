9099c2f1b970bea85871dcc06d013b903fd7172e
Štěpán Němec <stepan10nemec@gmail.com>
2021-01-17 14:22:50 +0100

    Button for data fetch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const axios = require('axios');
+
 function App() {
+  const [data, setData] = useState('No data received');
+
+  const retreiveData = () => {
+    setData('Retreiving data')
+
+    axios.get('https://api.github.com/users/mapbox')
+      .then((response) => {
+        setData(response.data.avatar_url)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Click to retreive data. 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={retreiveData}>
+          Request
+        </button>
+        <p>
+          {data}
+        </p>
       </header>
     </div>
   );
